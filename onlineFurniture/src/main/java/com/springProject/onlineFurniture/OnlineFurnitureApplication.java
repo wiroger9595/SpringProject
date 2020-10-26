27a3562ab9f636a1eb520b19d1550c1c3f6c2d5f
Robert Chen <wiroger9595@gmail.com>
2020-10-26 09:30:41 +0800

    Init commit spring boot application

---
+++ onlineFurniture/src/main/java/com/springProject/onlineFurniture/OnlineFurnitureApplication.java
@@ -0,0 +1,14 @@
+package com.springProject.onlineFurniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFurnitureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFurnitureApplication.class, args);
+		System.out.println("finish");
+	}
+
+}
---
+++ onlineFurniture/src/test/java/com/springProject/onlineFurniture/OnlineFurnitureApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springProject.onlineFurniture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFurnitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("success");
+	}
+
+}
